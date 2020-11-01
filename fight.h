38e807394c6344a6079ffba0c6023588d2f239c3
Jim Ries <RiesJ@missouri.edu>
2020-11-01 16:36:59 -0600

    Initial roll-in.

---
+++ fight.h
@@ -0,0 +1,2 @@
+void Fight( char * combatant1, int ac1, int hp1, int hitbonus1, char * attack1damage1, char * attack2damage1, 
+            char * combatant2, int ac2, int hp2, int hitbonus2, char * attack1damage2, char * attack2damage2);
